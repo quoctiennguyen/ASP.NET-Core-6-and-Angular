7408dcfeb64e2a3323be47cd3383f26a220bcdf5
Thomas <thnguyen@paritycorp.com>
2022-09-30 15:18:03 +0700

    Setup proxy base url

--- Chapter_02/HealthCheck/HealthCheck/src/app/app.component.ts
+++ Chapter_02/HealthCheck/HealthCheck/src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   public forecasts?: WeatherForecast[];
 
   constructor(http: HttpClient) {
-    http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
+    http.get<WeatherForecast[]>('/api/weatherforecast').subscribe(result => {
       this.forecasts = result;
     }, error => console.error(error));
   }
