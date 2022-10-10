4d62f1f87871b764c179c4667b56f77221665a84
Thomas <thnguyen@paritycorp.com>
2022-10-10 10:34:07 +0700

    Change css to scss

--- Chapter_02/HealthCheck/HealthCheck/src/app/app.module.ts
+++ Chapter_02/HealthCheck/HealthCheck/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HealthCheckComponent } from './health-check/health-check.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HealthCheckComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule
+    BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
