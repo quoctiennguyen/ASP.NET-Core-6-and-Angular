816ee24bc090532f3f9a46244a575d9a8cdbcb64
Thomas <thnguyen@paritycorp.com>
2022-10-12 11:54:52 +0700

    Install Epplus for import excel

---
+++ Chapter_02/HealthCheck/HealthCheckAPI/Controllers/SeedController.cs
@@ -0,0 +1,18 @@
+﻿using HealthCheckAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthCheckAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeedController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SeedController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
--- Chapter_02/HealthCheck/HealthCheckAPI/Program.cs
+++ Chapter_02/HealthCheck/HealthCheckAPI/Program.cs
@@ -1,4 +1,6 @@
 global using HealthCheckAPI;
+using HealthCheckAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add ApplicationDbContext and SQL Server support
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
