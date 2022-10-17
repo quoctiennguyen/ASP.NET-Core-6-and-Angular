c5a365732a3722426caed07ecca1b55527bb9739
Thomas <thnguyen@paritycorp.com>
2022-10-17 13:40:56 +0700

    Generic way to show mat-error - p1

--- Chapter_02/HealthCheck/HealthCheck/src/app/countries/country-edit.component.ts
+++ Chapter_02/HealthCheck/HealthCheck/src/app/countries/country-edit.component.ts
@@ -129,4 +129,28 @@ export class CountryEditComponent implements OnInit {
         }));
     }
   }
+
+  getErrors(
+    control: AbstractControl,
+    displayName: string,
+  ): string[] {
+    var errors: string[] = [];
+    Object.keys(control.errors || {}).forEach((key) => {
+      switch (key) {
+      case 'required':
+        errors.push('${displayName} is required.');
+        break;
+      case 'pattern':
+        errors.push('${displayName} contains invalid characters.');
+        break;
+      case 'isDupeField':
+        errors.push('${displayName} already exists: please choose another.');
+        break;
+      default:
+        errors.push('${displayName} is invalid.');
+        break;
+      }
+    });
+    return errors;
+  }
 }
