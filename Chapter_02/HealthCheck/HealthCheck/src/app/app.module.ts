788d63293b06fcef0b4819401ebdd58fa87ba1af
Thomas <thnguyen@paritycorp.com>
2022-10-17 11:46:58 +0700

    Using FormBuilder to build form

--- Chapter_02/HealthCheck/HealthCheck/src/app/app.module.ts
+++ Chapter_02/HealthCheck/HealthCheck/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AngularMaterialModule } from './angular-material.module';
 import { CountriesComponent } from './countries/countries.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CityEditComponent } from './cities/city-edit.component';
+import { CountryEditComponent } from './countries/country-edit.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { CityEditComponent } from './cities/city-edit.component';
     NavMenuComponent,
     CitiesComponent,
     CountriesComponent,
-    CityEditComponent
+    CityEditComponent,
+    CountryEditComponent
   ],
   imports: [
     BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule, AngularMaterialModule, ReactiveFormsModule
--- Chapter_02/HealthCheck/HealthCheckAPI/Controllers/CountriesController.cs
+++ Chapter_02/HealthCheck/HealthCheckAPI/Controllers/CountriesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -116,6 +117,36 @@ namespace HealthCheckAPI.Controllers
         private bool CountryExists(int id)
         {
             return _context.Countries.Any(e => e.Id == id);
+        }
+
+        [HttpPost]
+        [Route("IsDupeField")]
+        public bool IsDupeField(
+            int countryId,
+            string fieldName,
+            string fieldValue)
+        {
+            /* this approach look code is longer than Dynamic Core
+            switch (fieldName)
+            {
+                case "name":
+                    return _context.Countries.Any(
+                        c => c.Name == fieldValue && c.Id != countryId);
+                case "iso2":
+                    return _context.Countries.Any(
+                        c => c.ISO2 == fieldValue && c.Id != countryId);
+                case "iso3":
+                    return _context.Countries.Any(
+                        c => c.ISO3 == fieldValue && c.Id != countryId);
+                default:
+                    return false;
+            }
+            */
+            // Alternative approach (using System.Linq.Dynamic.Core)
+            return (ApiResult<Country>.IsValidProperty(fieldName, true)) && _context.Countries.Any(
+                $"{fieldName} == @0 && Id != @1",
+                fieldValue,
+                countryId);
         }
     }
 }
